31fc0ac8b1cb772dbb15ec36076dd80b0b16c186
ANG-Server <96008766+ANGJustinl@users.noreply.github.com>
2025-10-26 17:38:11 +0800

    v0.0.4

--- src/nonebot_plugin_markdown2img/config.py
+++ src/nonebot_plugin_markdown2img/config.py
@@ -1,9 +1,8 @@
-from typing import Optional
 from pydantic import BaseModel
 
 
 class Config(BaseModel):
-    markdown2img_font_path: Optional[str] = None
-    markdown2img_disable_gpu: Optional[bool] = True
-    markdown2img_disable_linkify: Optional[bool] = True
+    markdown2img_font_path: str | None = None
+    markdown2img_disable_gpu: bool | None = True
+    markdown2img_disable_linkify: bool | None = True
     pass
